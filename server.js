92946ff07be45c2f78297da2bbeb520002d5cdb2
CHAABANERamy <ramy.chaa@gmail.com>
2016-11-19 21:41:47 +0100

    p 1

---
+++ server.js
@@ -0,0 +1,15 @@
+git initconst bodyParser = require('body-parser');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
